03707e57c2c4b369ead0d9f9fd32f4b5f53a690c
muhsin bin irshad <69749879+muhsinbinirshad@users.noreply.github.com>
2023-06-28 07:33:22 -0500

    Add files via upload
    this is a file to install all tools used in main code

---
+++ install-tools.py
@@ -0,0 +1,18 @@
+import os
+
+tools = [
+    ("subfinder", "GO111MODULE=on go get -u github.com/projectdiscovery/subfinder/v2/cmd/subfinder"),
+    ("amass", "GO111MODULE=on go get -v github.com/OWASP/Amass/v3/..."),
+    ("sublist3r", "git clone https://github.com/aboul3la/Sublist3r.git && cd Sublist3r && pip install -r requirements.txt"),
+    ("httprobe", "go get -u github.com/tomnomnom/httprobe"),
+    ("knockpy", "git clone https://github.com/guelfoweb/knock.git && cd knock && python setup.py install")
+]
+
+for tool, command in tools:
+    print(f"Installing {tool}...")
+    if os.system(command) == 0:
+        print(f"{tool} installed successfully!")
+    else:
+        print(f"Failed to install {tool}.")
+
+print("All tools installed successfully!")
